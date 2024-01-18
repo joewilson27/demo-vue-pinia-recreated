a0b90a3734b942c18bbe6fdbe96fc0c61117f0aa
Joe Wilson <wilsonmangunsong@gmail.com>
2024-01-18 12:06:04 +0700

    first-commit
    first commit

---
+++ vue-pinia-recreated/src/stores/post.js
@@ -0,0 +1,19 @@
+import { defineStore } from "pinia"
+
+export const usePostStore = defineStore({
+  id: "post",
+  state: () => ({
+    posts: [],
+    post: null,
+    loading: false,
+    error: null
+  }),
+  getters: {
+    getPostsPerAuthor: (state) => {
+      return (authorId) => state.posts.filter((post) => post.userId === authorId)
+    }
+  },
+  actions: {
+    
+  }
+})
